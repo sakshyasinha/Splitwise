5bf7779fb8926cfac73ab9362493e62de21ce890
sakshyasinha <sakshyasinha32@gmail.com>
2026-04-27 15:54:04 +0530

    fixing group membership

--- server/services/group.service.js
+++ server/services/group.service.js
@@ -1,14 +1,55 @@
 import Group from "../models/group.model.js";
+import User from "../models/user.model.js";
+import mongoose from "mongoose";
 
-export const createGroup = async ({ name, userId }) => {
+const resolveMemberUsers = async (members = []) => {
+  const cleaned = [...new Set((members || []).map((item) => String(item).trim()).filter(Boolean))];
+  const users = [];
+  const missing = [];
+
+  for (const identifier of cleaned) {
+    let user = null;
+
+    if (mongoose.Types.ObjectId.isValid(identifier)) {
+      user = await User.findById(identifier);
+    }
+
+    if (!user) {
+      user = await User.findOne({ email: identifier.toLowerCase() });
+    }
+
+    if (user) {
+      users.push(user);
+    } else {
+      missing.push(identifier);
+    }
+  }
+
+  if (missing.length > 0) {
+    const error = new Error(`Users not found for: ${missing.join(", ")}`);
+    error.statusCode = 400;
+    throw error;
+  }
+
+  return users;
+};
+
+export const createGroup = async ({ name, userId, members = [] }) => {
   if (!name) {
     const error = new Error("Group name is required");
     error.statusCode = 400;
     throw error;
   }
 
+  if (!userId) {
+    const error = new Error("User is required to create a group");
+    error.statusCode = 400;
+    throw error;
+  }
+
   const trimmedName = name.trim();
-  const normalizedMembers = userId ? [userId] : [];
+  const resolvedMembers = await resolveMemberUsers(members);
+  const normalizedMembers = [...new Set([String(userId), ...resolvedMembers.map((user) => String(user._id))])];
 
   const existingGroup = await Group.findOne({
     name: trimmedName,
@@ -30,6 +71,6 @@ export const createGroup = async ({ name, userId }) => {
   return await Group.create({
     name: trimmedName,
     members: normalizedMembers,
-    createdBy: userId,
+    createdBy: [userId],
   });
 };
