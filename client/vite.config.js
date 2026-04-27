531c7f5829624aea3a9b4d06fd82c1422c16d70f
sakshyasinha <sakshyasinha32@gmail.com>
2026-04-27 09:50:56 +0530

    fixing the base path for GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/splitwise/' : '/',
+  base: '/splitwise/', // Ensure the base path is set for GitHub Pages
   plugins: [react()],
   server: {
     port: 5173
